d315353236901fa970dd8f650e55eeefa96bab51
moe453 <107380496+moe453@users.noreply.github.com>
2022-06-13 00:19:14 -0400

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,8 @@
+#1/usr/bin/env python3
+
+def calculate_age ():
+    birth_year = input('What year were you born? ')
+    current_year = 2022
+    age = birth_year - current_year
+    return age
+    
