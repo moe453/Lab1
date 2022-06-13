df2ffd457f196819d7b9307ed7afa9ab84c2cf58
moe453 <107380496+moe453@users.noreply.github.com>
2022-06-13 01:05:09 -0400

    Lab1-1 change

--- lab1.py
+++ lab1.py
@@ -1,9 +1,12 @@
 #1/usr/bin/env python3
 
 def calculate_age ():
-    birth_year = input('What year were you born? ')
-    current_year = 2022
-    age = birth_year - current_year
+    try:
+        birth_year = input('What year were you born? ')
+        current_year = 2022
+        age = birth_year - current_year
+    except:
+        print('Please enter an int')
     return age
 
 def helloWorld():
